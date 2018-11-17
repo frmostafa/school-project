1cd4bf845d4acb3829ac5c233104f27440468801
hassanhs1997 <hassan2013.sedighain@gmail.com>
2018-11-17 10:08:14 +0330

    add ui to the resource file and create users controller

---
+++ resources/views/admin/partials/nav.blade.php
@@ -0,0 +1,30 @@
+<header class="header white-bg">
+                   <div class="sidebar-toggle-box">
+                       <div data-original-title="Toggle Navigation" data-placement="right" class="icon-reorder tooltips"></div>
+                   </div>
+                   <!--logo start-->
+                   <a href="#" class="logo">مدرسه<span>دانش</span></a>
+
+                   <div class="top-nav ">
+
+                       <ul class="nav pull-right top-menu">
+
+                           <!-- user login dropdown start-->
+                           <li class="dropdown">
+                               <a data-toggle="dropdown" class="dropdown-toggle" href="#">
+                                   <img alt="" src="img/avatar1_small.jpg">
+                                   <span class="username">حسن صدیقیان</span>
+                                   <b class="caret"></b>
+                               </a>
+                               <ul class="dropdown-menu extended logout">
+                                   <div class="log-arrow-up"></div>
+                                   <li><a href="#"><i class=" icon-suitcase"></i>پروفایل</a></li>
+
+                                   <li><a href="login.html"><i class="icon-key"></i> خروج</a></li>
+                               </ul>
+                           </li>
+                           <!-- user login dropdown end -->
+                       </ul>
+                       <!--search & user info end-->
+                   </div>
+               </header>
---
+++ resources/views/admin/partials/sidebar.blade.php
@@ -0,0 +1,43 @@
+<aside>
+         <div id="sidebar"  class="nav-collapse ">
+             <!-- sidebar menu start-->
+             <ul class="sidebar-menu">
+                 <li class="active">
+                     <a class="" href="index.html">
+                         <i class="icon-dashboard"></i>
+                         <span>صفحه اصلی</span>
+                     </a>
+                 </li>
+                 <li class="sub-menu">
+                     <a href="javascript:;" class="">
+                         <i class="icon-book"></i>
+                         <span>معلمین</span>
+                         <span class="arrow"></span>
+                     </a>
+                     <ul class="sub">
+                         <li><a class="" href="dynamic_teacher_table.html">مشخصات معلمین</a></li>
+
+                     </ul>
+                 </li>
+                 <li class="sub-menu">
+                     <a href="javascript:;" class="">
+                         <i class="icon-cogs"></i>
+                         <span>دانش آموزان</span>
+                         <span class="arrow"></span>
+                     </a>
+                     <ul class="sub">
+                         <li><a class="" href="dynamic_student_table.html">مشخصات دانش آموزان</a></li>
+
+                     </ul>
+                 </li>
+
+                 <li>
+                     <a class="" href="login.html">
+                         <i class="icon-user"></i>
+                         <span>صفحه ورود به سایت</span>
+                     </a>
+                 </li>
+             </ul>
+             <!-- sidebar menu end-->
+         </div>
+     </aside>
